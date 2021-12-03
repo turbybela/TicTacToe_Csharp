ded0fcb20ae7c31ab6543dd2bab6e30769045f08
Turby <Turby@TURBYGEPE>
2021-12-03 20:39:18 +0100

    Add project files.

---
+++ source/Form1.Designer.cs
@@ -0,0 +1,405 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.p32 = new System.Windows.Forms.Panel();
+            this.label_32 = new System.Windows.Forms.Label();
+            this.p22 = new System.Windows.Forms.Panel();
+            this.label_22 = new System.Windows.Forms.Label();
+            this.p12 = new System.Windows.Forms.Panel();
+            this.label_12 = new System.Windows.Forms.Label();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.p33 = new System.Windows.Forms.Panel();
+            this.label_33 = new System.Windows.Forms.Label();
+            this.p23 = new System.Windows.Forms.Panel();
+            this.label_23 = new System.Windows.Forms.Label();
+            this.p13 = new System.Windows.Forms.Panel();
+            this.label_13 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.p31 = new System.Windows.Forms.Panel();
+            this.label_31 = new System.Windows.Forms.Label();
+            this.p21 = new System.Windows.Forms.Panel();
+            this.label_21 = new System.Windows.Forms.Label();
+            this.p11 = new System.Windows.Forms.Panel();
+            this.label_11 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.p32.SuspendLayout();
+            this.p22.SuspendLayout();
+            this.p12.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.p33.SuspendLayout();
+            this.p23.SuspendLayout();
+            this.p13.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.p31.SuspendLayout();
+            this.p21.SuspendLayout();
+            this.p11.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel6);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 61);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(450, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.p32);
+            this.panel4.Controls.Add(this.p22);
+            this.panel4.Controls.Add(this.p12);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(150, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(150, 450);
+            this.panel4.TabIndex = 3;
+            // 
+            // p32
+            // 
+            this.p32.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.p32.Controls.Add(this.label_32);
+            this.p32.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.p32.Location = new System.Drawing.Point(0, 300);
+            this.p32.Name = "p32";
+            this.p32.Size = new System.Drawing.Size(150, 150);
+            this.p32.TabIndex = 2;
+            // 
+            // label_32
+            // 
+            this.label_32.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label_32.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label_32.Location = new System.Drawing.Point(0, 0);
+            this.label_32.Name = "label_32";
+            this.label_32.Size = new System.Drawing.Size(146, 146);
+            this.label_32.TabIndex = 3;
+            this.label_32.Text = "-";
+            this.label_32.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // p22
+            // 
+            this.p22.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.p22.Controls.Add(this.label_22);
+            this.p22.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.p22.Location = new System.Drawing.Point(0, 150);
+            this.p22.Name = "p22";
+            this.p22.Size = new System.Drawing.Size(150, 300);
+            this.p22.TabIndex = 1;
+            // 
+            // label_22
+            // 
+            this.label_22.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label_22.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label_22.Location = new System.Drawing.Point(0, 0);
+            this.label_22.Name = "label_22";
+            this.label_22.Size = new System.Drawing.Size(146, 146);
+            this.label_22.TabIndex = 1;
+            this.label_22.Text = "-";
+            this.label_22.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // p12
+            // 
+            this.p12.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.p12.Controls.Add(this.label_12);
+            this.p12.Dock = System.Windows.Forms.DockStyle.Top;
+            this.p12.Location = new System.Drawing.Point(0, 0);
+            this.p12.Name = "p12";
+            this.p12.Size = new System.Drawing.Size(150, 150);
+            this.p12.TabIndex = 0;
+            // 
+            // label_12
+            // 
+            this.label_12.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label_12.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label_12.Location = new System.Drawing.Point(0, 0);
+            this.label_12.Name = "label_12";
+            this.label_12.Size = new System.Drawing.Size(146, 146);
+            this.label_12.TabIndex = 1;
+            this.label_12.Text = "-";
+            this.label_12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.p33);
+            this.panel6.Controls.Add(this.p23);
+            this.panel6.Controls.Add(this.p13);
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel6.Location = new System.Drawing.Point(300, 0);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(150, 450);
+            this.panel6.TabIndex = 2;
+            // 
+            // p33
+            // 
+            this.p33.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.p33.Controls.Add(this.label_33);
+            this.p33.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.p33.Location = new System.Drawing.Point(0, 300);
+            this.p33.Name = "p33";
+            this.p33.Size = new System.Drawing.Size(150, 150);
+            this.p33.TabIndex = 2;
+            // 
+            // label_33
+            // 
+            this.label_33.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label_33.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label_33.Location = new System.Drawing.Point(0, 0);
+            this.label_33.Name = "label_33";
+            this.label_33.Size = new System.Drawing.Size(146, 146);
+            this.label_33.TabIndex = 3;
+            this.label_33.Text = "-";
+            this.label_33.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // p23
+            // 
+            this.p23.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.p23.Controls.Add(this.label_23);
+            this.p23.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.p23.Location = new System.Drawing.Point(0, 150);
+            this.p23.Name = "p23";
+            this.p23.Size = new System.Drawing.Size(150, 300);
+            this.p23.TabIndex = 1;
+            // 
+            // label_23
+            // 
+            this.label_23.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label_23.Location = new System.Drawing.Point(0, 0);
+            this.label_23.Name = "label_23";
+            this.label_23.Size = new System.Drawing.Size(150, 150);
+            this.label_23.TabIndex = 1;
+            this.label_23.Text = "-";
+            this.label_23.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // p13
+            // 
+            this.p13.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.p13.Controls.Add(this.label_13);
+            this.p13.Dock = System.Windows.Forms.DockStyle.Top;
+            this.p13.Location = new System.Drawing.Point(0, 0);
+            this.p13.Name = "p13";
+            this.p13.Size = new System.Drawing.Size(150, 150);
+            this.p13.TabIndex = 0;
+            // 
+            // label_13
+            // 
+            this.label_13.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label_13.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label_13.Location = new System.Drawing.Point(0, 0);
+            this.label_13.Name = "label_13";
+            this.label_13.Size = new System.Drawing.Size(146, 146);
+            this.label_13.TabIndex = 1;
+            this.label_13.Text = "-";
+            this.label_13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.p31);
+            this.panel3.Controls.Add(this.p21);
+            this.panel3.Controls.Add(this.p11);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(150, 450);
+            this.panel3.TabIndex = 0;
+            // 
+            // p31
+            // 
+            this.p31.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.p31.Controls.Add(this.label_31);
+            this.p31.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.p31.Location = new System.Drawing.Point(0, 300);
+            this.p31.Name = "p31";
+            this.p31.Size = new System.Drawing.Size(150, 150);
+            this.p31.TabIndex = 2;
+            // 
+            // label_31
+            // 
+            this.label_31.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label_31.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label_31.Location = new System.Drawing.Point(0, 0);
+            this.label_31.Name = "label_31";
+            this.label_31.Size = new System.Drawing.Size(146, 146);
+            this.label_31.TabIndex = 3;
+            this.label_31.Text = "-";
+            this.label_31.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // p21
+            // 
+            this.p21.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.p21.Controls.Add(this.label_21);
+            this.p21.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.p21.Location = new System.Drawing.Point(0, 150);
+            this.p21.Name = "p21";
+            this.p21.Size = new System.Drawing.Size(150, 300);
+            this.p21.TabIndex = 1;
+            // 
+            // label_21
+            // 
+            this.label_21.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label_21.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label_21.Location = new System.Drawing.Point(0, 0);
+            this.label_21.Name = "label_21";
+            this.label_21.Size = new System.Drawing.Size(146, 146);
+            this.label_21.TabIndex = 1;
+            this.label_21.Text = "-";
+            this.label_21.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // p11
+            // 
+            this.p11.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.p11.Controls.Add(this.label_11);
+            this.p11.Dock = System.Windows.Forms.DockStyle.Top;
+            this.p11.Location = new System.Drawing.Point(0, 0);
+            this.p11.Name = "p11";
+            this.p11.Size = new System.Drawing.Size(150, 150);
+            this.p11.TabIndex = 0;
+            // 
+            // label_11
+            // 
+            this.label_11.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label_11.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label_11.Location = new System.Drawing.Point(0, 0);
+            this.label_11.Name = "label_11";
+            this.label_11.Size = new System.Drawing.Size(146, 146);
+            this.label_11.TabIndex = 0;
+            this.label_11.Text = "-";
+            this.label_11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnNew);
+            this.panel2.Controls.Add(this.title);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(450, 61);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnNew
+            // 
+            this.btnNew.BackColor = System.Drawing.SystemColors.Info;
+            this.btnNew.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnNew.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnNew.Location = new System.Drawing.Point(335, 9);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(103, 37);
+            this.btnNew.TabIndex = 1;
+            this.btnNew.Text = "New Game";
+            this.btnNew.UseVisualStyleBackColor = false;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.title.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.title.Location = new System.Drawing.Point(125, 9);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(202, 39);
+            this.title.TabIndex = 0;
+            this.title.Text = "Tic-Tac-Toe";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(450, 511);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(466, 550);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(466, 550);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TicTacToe!";
+            this.panel1.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.p32.ResumeLayout(false);
+            this.p22.ResumeLayout(false);
+            this.p12.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.p33.ResumeLayout(false);
+            this.p23.ResumeLayout(false);
+            this.p13.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.p31.ResumeLayout(false);
+            this.p21.ResumeLayout(false);
+            this.p11.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel p32;
+        private System.Windows.Forms.Panel p22;
+        private System.Windows.Forms.Panel p12;
+        private System.Windows.Forms.Panel p33;
+        private System.Windows.Forms.Panel p23;
+        private System.Windows.Forms.Panel p13;
+        private System.Windows.Forms.Panel p31;
+        private System.Windows.Forms.Panel p21;
+        private System.Windows.Forms.Panel p11;
+        private System.Windows.Forms.Label label_12;
+        private System.Windows.Forms.Label label_13;
+        private System.Windows.Forms.Label label_22;
+        private System.Windows.Forms.Label label_21;
+        private System.Windows.Forms.Label label_32;
+        private System.Windows.Forms.Label label_33;
+        private System.Windows.Forms.Label label_23;
+        private System.Windows.Forms.Label label_31;
+        private System.Windows.Forms.Label label_11;
+    }
+}
+
---
+++ source/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        Label[] labels = new Label[9];
+
+        bool prev = true; // true: X / false: O
+
+        int clickNum = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            labels = new Label[9] {
+                label_11, label_12, label_13,
+                label_21, label_22, label_23,
+                label_31, label_32, label_33
+            };
+
+            foreach (var l in labels)
+            {
+                l.Click += new EventHandler(Clicked);
+            }
+
+            reset();
+        }
+
+        private Label GetLabel(int pos)
+        {
+            int pY = (pos - (pos % 10)) / 10;
+            int pX = pos % 10;
+            switch (pY)
+            {
+                case 1:
+                    switch (pX)
+                    {
+                        case 1:
+                            return labels[0];
+                        case 2:
+                            return labels[1];
+                        case 3:
+                            return labels[2];
+                        default:
+                            return null;
+                    }
+                case 2:
+                    switch (pX)
+                    {
+                        case 1:
+                            return labels[3];
+                        case 2:
+                            return labels[4];
+                        case 3:
+                            return labels[5];
+                        default:
+                            return null;
+                    }
+                case 3:
+                    switch (pX)
+                    {
+                        case 1:
+                            return labels[6];
+                        case 2:
+                            return labels[7];
+                        case 3:
+                            return labels[8];
+                        default:
+                            return null;
+                    }
+                default:
+                    return null;
+            }
+        }
+
+        void reset()
+        {
+            clickNum = 0;
+            foreach (var l in labels)
+            {
+                l.Text = "-";
+            }
+        }
+
+
+        private void Win(string winner)
+        {
+            string message = "Nil";
+            if (winner == "Tie")
+            {
+                message = "Its a Tie!";
+            }
+            else
+            {
+                message = "The winner is: " + winner + "!";
+            }
+
+            MessageBox.Show(message, "Game Over!");
+            reset();
+        }
+
+
+        private KeyValuePair<bool, string> Check()
+        {
+            #region help
+
+            /* Possible wins:
+             * 
+                XXX * 3
+             *
+                X
+                X
+                X * 3
+             *
+                X
+                 X
+                  X
+             *
+                  X
+                 X
+                X
+             */// new KeyValuePair<bool, string>(false, null);
+
+            #endregion
+
+            for (int t = 0; t < 2; t++)
+            {
+                string target = (t == 0 ? "X" : "O");
+                for (int i = 1; i <= 3; i++) // horizontal
+                {
+                    int pY = i * 10;
+
+                    if (GetLabel(pY + 1).Text == target &&
+                        GetLabel(pY + 2).Text == target &&
+                        GetLabel(pY + 3).Text == target) 
+                        {
+                            return new KeyValuePair<bool, string>(true, target);
+                    }
+                }
+                for (int i = 1; i <= 3; i++) // vertical
+                {
+                    int pX = i;
+
+                    if (GetLabel(pX + 10).Text == target &&
+                        GetLabel(pX + 20).Text == target &&
+                        GetLabel(pX + 30).Text == target)
+                    {
+                        return new KeyValuePair<bool, string>(true, target);
+                    }
+                }
+
+                if (GetLabel(11).Text == target && // Diag 1
+                    GetLabel(22).Text == target &&
+                    GetLabel(33).Text == target)
+                    {
+                        return new KeyValuePair<bool, string>(true, target);
+                    }
+
+                if (GetLabel(13).Text == target && // Diag 2
+                    GetLabel(22).Text == target &&
+                    GetLabel(31).Text == target)
+                    {
+                        return new KeyValuePair<bool, string>(true, target);
+                    }
+            }
+
+            if (clickNum == 9) return new KeyValuePair<bool, string>(true, "Tie");
+
+            return new KeyValuePair<bool, string>(false, null); // no winner
+        }
+
+        private void Clicked(object sender, EventArgs e)
+        {
+
+            Label s = (Label)sender;
+
+            int pos = int.Parse(s.Name.Split('_')[1]);
+
+            if (s.Text != "-") return;
+
+            s.Text = (prev?"X":"O");
+            prev = !prev;
+            clickNum++;
+
+            KeyValuePair<bool, string> c = Check();
+            if (c.Key) Win(c.Value);
+        }
+
+
+
+
+        private void btnNew_Click(object sender, EventArgs e)
+        { 
+            reset();
+        }
+    }
+}
